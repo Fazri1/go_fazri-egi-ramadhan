9cd0c4507cd76168653a1cf743e383594be8a982
Fazri Egi <71480242+Fazri1@users.noreply.github.com>
2023-03-11 16:06:36 +0700

    update prioritas 2

--- 12_Concurrent Programing/praktikum/prioritas_2/main.go
+++ 12_Concurrent Programing/praktikum/prioritas_2/main.go
@@ -3,33 +3,44 @@
 package main
 
 import (
-	"fmt"
+	"runtime"
 	"sync"
+	"time"
+	"fmt"
 )
 
 var wg sync.WaitGroup
 
 func frekuensiHuruf(text string, startIndex, endIndex int, ch chan map[string]int) {
-    defer wg.Done()
-    huruf := map[string]int{}
+	runtime.LockOSThread()
+	defer wg.Done()
+	
+	huruf := map[string]int{}
 
     for _, char := range text[startIndex:endIndex] {
         if char > 65 && char < 90 || char > 97 && char < 122 {
             huruf[string(char)] += 1
         }
     }
-    ch <- huruf
+	ch <- huruf
+
+	time.Sleep(time.Second * 2)
+	runtime.UnlockOSThread()
+}
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
-    text := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
+	text := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
 
     ch := make(chan map[string]int)
     result := make(map[string]int)
-    goroutine := 5
+	goroutine := 5
     goroutineSize := len(text) / goroutine
-    
-    for i := 0; i < goroutine; i++ {
+	
+	for i := 0; i < goroutine; i++ {
         start := i * goroutineSize
         end := start + goroutineSize
         if i == goroutine-1 {
@@ -40,19 +51,19 @@ func main() {
         go frekuensiHuruf(text, start, end, ch)
     }
 
-    go func() {
+	go func() {
         wg.Wait()
         close(ch)
     }()
 
-    for hasilGoroutine := range ch {
+	// gabungkan semua hasil
+	for hasilGoroutine := range ch {
         for char, count := range hasilGoroutine {
             result[char] += count
         }
     }
-
-    for char, count := range result {
-        fmt.Println(char + ":", count)
-    }
-    
+	
+	for char, count := range result {
+		fmt.Println(char + ":", count)
+	}
 }
