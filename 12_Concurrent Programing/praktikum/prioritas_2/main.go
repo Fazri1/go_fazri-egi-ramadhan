1733b29fbc5b464c1737cc2e040796a17164833d
Fazri Egi <71480242+Fazri1@users.noreply.github.com>
2023-03-11 16:12:18 +0700

    merapihkan main.go prioritas 2

--- 12_Concurrent Programing/praktikum/prioritas_2/main.go
+++ 12_Concurrent Programing/praktikum/prioritas_2/main.go
@@ -17,11 +17,11 @@ func frekuensiHuruf(text string, startIndex, endIndex int, ch chan map[string]in
 	
 	huruf := map[string]int{}
 
-    for _, char := range text[startIndex:endIndex] {
-        if char > 65 && char < 90 || char > 97 && char < 122 {
-            huruf[string(char)] += 1
-        }
-    }
+	for _, char := range text[startIndex:endIndex] {
+        	if char > 65 && char < 90 || char > 97 && char < 122 {
+            		huruf[string(char)] += 1
+        	}
+	}
 	ch <- huruf
 
 	time.Sleep(time.Second * 2)
@@ -35,33 +35,33 @@ func init() {
 func main() {
 	text := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
 
-    ch := make(chan map[string]int)
-    result := make(map[string]int)
+	ch := make(chan map[string]int)
+	result := make(map[string]int)
 	goroutine := 5
-    goroutineSize := len(text) / goroutine
+    	goroutineSize := len(text) / goroutine
 	
 	for i := 0; i < goroutine; i++ {
-        start := i * goroutineSize
-        end := start + goroutineSize
-        if i == goroutine-1 {
-            end = len(text)
-        }
+        	start := i * goroutineSize
+        	end := start + goroutineSize
+        	if i == goroutine-1 {
+            		end = len(text)
+        	}
 
-        wg.Add(1)
-        go frekuensiHuruf(text, start, end, ch)
-    }
+        	wg.Add(1)
+        	go frekuensiHuruf(text, start, end, ch)
+    	}
 
 	go func() {
-        wg.Wait()
-        close(ch)
-    }()
+        	wg.Wait()
+        	close(ch)
+    	}()
 
 	// gabungkan semua hasil
 	for hasilGoroutine := range ch {
-        for char, count := range hasilGoroutine {
-            result[char] += count
-        }
-    }
+        	for char, count := range hasilGoroutine {
+            		result[char] += count
+        	}
+    	}
 	
 	for char, count := range result {
 		fmt.Println(char + ":", count)
